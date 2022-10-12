64b853bfd9935fbff5e3a64b916a24128c093fa3
Humberto Romero <hipopo@gmail.com>
2022-10-11 22:42:34 -0500

    Add files via upload

---
+++ env/conn-temp.php
@@ -0,0 +1,33 @@
+<?php
+	
+	//require 'config.php';
+
+	/**
+		 * Conexion a BD
+		 */
+		class Conexion extends PDO {
+
+		   private $tipo_de_base    = 'mysql';          /**< Indica el tipo de motor de datos */
+		   private $host            = '####';      /**< Indica el host */
+		   private $nombre_de_base 	= '####';           /**< Indica el nombre de la base de datos */
+		   private $usuario         = '####';           /**< Indica el nombre de usuario de la base de datos */
+		   private $contrasena      = '####';       /**< Indica la contraseña de usuario de la base de datos */
+
+
+		   /**
+		     * @brief crea la conexión PDO.
+		    */  
+		   public function __CONSTRUCT() {
+
+		      try{
+		         parent::__CONSTRUCT($this->tipo_de_base.':host='.$this->host.';dbname='.$this->nombre_de_base, $this->usuario, $this->contrasena);
+
+		      }catch(PDOException $e){
+		         echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
+		         exit;
+		      }
+		   } 
+		}
+	
+	
+?>
